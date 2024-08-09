60698d0d4d1a24aec195310228615e752c4b38a1
PeterChen712 <rudypeter@gmail.com>
2024-08-09 15:25:11 +0800

    chore: Seed waste categories and admin user in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\WasteCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MaterialCategorySeeder::class,
             MaterialSeeder::class,
+            AdminUserSeeder::class,
+            WasteCategoriesSeeder::class,
         ]);
     }
 }
